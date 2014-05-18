849a215c28536dd0fe54127245f5aa4399840392
Matt Brennan <mattyb1000@gmail.com>
2014-05-18 10:48:20 +0100

    formatting

--- test.js
+++ test.js
@@ -1,16 +1,17 @@
 var laws = require('laws');
 var σ = require('./lib');
 
-[laws.monoid.leftIdentity,
- laws.monoid.rightIdentity,
- laws.semigroup.associativity,
- laws.functor.identity,
- laws.functor.composition,
- laws.applicative.identity,
- laws.applicative.composition,
- laws.applicative.homomorphism,
- laws.applicative.interchange,
- laws.chain.associativity,
- laws.monad.leftIdentity,
- laws.monad.rightIdentity].forEach(function(l) { l(σ).asTest()() });
+[ laws.monoid      .leftIdentity
+, laws.monoid      .rightIdentity
+, laws.semigroup   .associativity
+, laws.functor     .identity
+, laws.functor     .composition
+, laws.applicative .identity
+, laws.applicative .composition
+, laws.applicative .homomorphism
+, laws.applicative .interchange
+, laws.chain       .associativity
+, laws.monad       .leftIdentity
+, laws.monad       .rightIdentity
+].forEach(function(l) { l(σ).asTest()() });
 
